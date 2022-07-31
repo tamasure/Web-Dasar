29862248f3e6df238a7090d3b14ed8ce47e1c269
tamasure <pratamamrandy@gmail.com>
2022-07-31 13:24:24 +0700

    submision

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+function moreArtikel1() {
+    var x = document.getElementById("more1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function moreArtikel2() {
+    var x = document.getElementById("more2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function moreArtikel3() {
+    var x = document.getElementById("more3");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
